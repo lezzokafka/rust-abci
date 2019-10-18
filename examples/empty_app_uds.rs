c0f8d22527cbf39dd9a847c4086c6992456dcf60
Leslie@crypto.com <lesliecheung@Lezzo-15.local>
2019-10-18 10:50:11 +0800

    adding empty_app_uds.rs

---
+++ examples/empty_app_uds.rs
@@ -0,0 +1,18 @@
+extern crate abci;
+extern crate env_logger;
+
+use env_logger::Env;
+
+// Simple example that responds with defaults to Tendermint
+struct EmptyApp;
+
+// Implement the Application and use default responses
+impl abci::Application for EmptyApp {}
+
+fn main() {
+    // Use default local addr and Tendermint ABCI port
+     let addr = "/tmp/unix.socket";
+    // Fire it up!
+    env_logger::from_env(Env::default().default_filter_or("info")).init();
+    abci::run(addr, EmptyApp);
+}
